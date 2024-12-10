b9df29ed4249814b1bacfd928aae29a2dbc26cdb
Leigh Hobson <leighhobson89@aol.com>
2024-12-10 14:03:48 +0100

    Initial commit

---
+++ constantsAndGlobalVars.js
@@ -0,0 +1,165 @@
+//DEBUG
+export let debugFlag = false;
+export let debugOptionFlag = false;
+export let stateLoading = false;
+
+//ELEMENTS
+let elements;
+let localization = {};
+let language = 'en';
+let languageSelected = 'en';
+let oldLanguage = 'en';
+
+//CONSTANTS
+export let gameState;
+export const MENU_STATE = 'menuState';
+export const GAME_VISIBLE_PAUSED = 'gameVisiblePaused';
+export const GAME_VISIBLE_ACTIVE = 'gameVisibleActive';
+
+//GLOBAL VARIABLES
+
+//FLAGS
+let audioMuted;
+let languageChangedFlag;
+let beginGameState = true;
+let gameInProgress = false;
+
+let autoSaveOn = false;
+export let pauseAutoSaveCountdown = true;
+
+//GETTER SETTER METHODS
+export function setElements() {
+    elements = {
+        menu: document.getElementById('menu'),
+        menuTitle: document.getElementById('menuTitle'),
+        newGameMenuButton: document.getElementById('newGame'),
+        returnToMenuButton: document.getElementById('returnToMenu'),
+        canvas: document.getElementById('canvas'),
+        canvasContainer: document.getElementById('canvasContainer'),
+        buttonRow: document.getElementById('buttonRow'),
+        overlay: document.getElementById('overlay'),
+        button1: document.getElementById('button1'),
+        button2: document.getElementById('button2')
+    };
+}
+
+export function setGameStateVariable(value) {
+    gameState = value;
+}
+
+export function getGameStateVariable() {
+    return gameState;
+}
+
+export function getElements() {
+    return elements;
+}
+
+export function getLanguageChangedFlag() {
+    return languageChangedFlag;
+}
+
+export function setLanguageChangedFlag(value) {
+    languageChangedFlag = value;
+}
+
+export function resetAllVariables() {
+    // GLOBAL VARIABLES
+
+    // FLAGS
+}
+
+export function captureGameStatusForSaving() {
+    let gameState = {};
+
+    // Game variables
+
+    // Flags
+
+    // UI elements
+
+    gameState.language = getLanguage();
+
+    return gameState;
+}
+export function restoreGameStatus(gameState) {
+    return new Promise((resolve, reject) => {
+        try {
+            // Game variables
+
+            // Flags
+
+            // UI elements
+
+            setLanguage(gameState.language);
+
+            resolve();
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+export function setLocalization(value) {
+    localization = value;
+}
+
+export function getLocalization() {
+    return localization;
+}
+
+export function setLanguage(value) {
+    language = value;
+}
+
+export function getLanguage() {
+    return language;
+}
+
+export function setOldLanguage(value) {
+    oldLanguage = value;
+}
+
+export function getOldLanguage() {
+    return oldLanguage;
+}
+
+export function setAudioMuted(value) {
+    audioMuted = value;
+}
+
+export function getAudioMuted() {
+    return audioMuted;
+}
+
+export function getMenuState() {
+    return MENU_STATE;
+}
+
+export function getGameVisibleActive() {
+    return GAME_VISIBLE_ACTIVE;
+}
+
+export function getLanguageSelected() {
+    return languageSelected;
+}
+
+export function setLanguageSelected(value) {
+    languageSelected = value;
+}
+
+export function getBeginGameStatus() {
+    return beginGameState;
+}
+
+export function setBeginGameStatus(value) {
+    beginGameState = value;
+}
+
+export function getGameInProgress() {
+    return gameInProgress;
+}
+
+export function setGameInProgress(value) {
+    gameInProgress = value;
+}
