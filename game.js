6eeff3e95abfb1c6d6ed16916d06f7fb55c5bbe0
Leigh Hobson <leighhobson89@aol.com>
2025-02-28 00:19:02 +0100

    moved timer manager to its own module

--- game.js
+++ game.js
@@ -187,87 +187,9 @@ import {
  import { initializeAutoSave, saveGame } from './saveLoadGame.js';
  import { drawTab5Content } from './drawTab5Content.js';
  import { sfxPlayer, weatherAmbienceManager, backgroundAudio } from './audioManager.js';
+ import { timerManager } from './timerManager.js';
 
-//---------------------------------------------------------------------------------------------------------
-class TimerManager {
-    constructor() {
-        this.timers = new Map();
-    }
-
-    addTimer(key, duration, onExpire) {
-        if (this.timers.has(key)) {
-            return;
-        }
-        const timer = new Timer(duration, onExpire);
-        this.timers.set(key, timer);
-        timer.start();
-    }
-
-    removeTimer(key) {
-        if (this.timers.has(key)) {
-            this.timers.get(key).stop();
-            this.timers.delete(key);
-        }
-    }
-
-    stopAllTimers() {
-        this.timers.forEach(timer => timer.stop());
-    }
-
-    getTimer(key) {
-        return this.timers.get(key);
-    }
-}
-
-class Timer {
-    constructor(duration, onExpire) {
-        this.duration = duration;
-        this.onExpire = onExpire;
-        this.timerId = null;
-        this.elapsedTime = 0;
-        this.isPaused = false;
-    }
-
-    start() {
-        if (this.isPaused) {
-            this.resume();
-        } else {
-            this.timerId = setInterval(() => {
-                this.elapsedTime += this.duration;
-                this.onExpire();
-            }, this.duration);
-        }
-    }
-
-    stop() {
-        if (this.timerId) {
-            clearInterval(this.timerId);
-            this.timerId = null;
-        }
-    }
-
-    pause() {
-        if (this.timerId) {
-            clearInterval(this.timerId);
-            this.isPaused = true;  // Mark as paused
-        }
-    }
-
-    resume() {
-        if (this.isPaused) {
-            this.isPaused = false;
-            this.timerId = setInterval(() => {
-                this.elapsedTime += this.duration;
-                this.onExpire();
-            }, this.duration);
-        }
-    }
-}
-
-export const timerManager = new TimerManager();
 //--------------------------------------------------------------------------------------------------------
-
-
 export function startGame() {
     setGameState(getGameVisibleActive());
     updateContent('Resources', `tab1`, 'intro');
