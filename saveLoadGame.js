ad106cf8a9f6e9622c158b5494c72b3a7198205e
Leigh <leighhobson89@aol.com>
2025-04-07 16:57:27 +0200

    .

--- saveLoadGame.js
+++ saveLoadGame.js
@@ -57,8 +57,6 @@ export function initializeAutoSave() {
 export async function saveGameToCloud(gameData, type) {
     try {
         const userId = getSaveName();
-        const currentTimestamp = new Date().toISOString();
-
         const { data: existingData, error: fetchError } = await supabase
             .from('CosmicForge_saves')
             .select('*')
@@ -74,7 +72,6 @@ export async function saveGameToCloud(gameData, type) {
                 .from('CosmicForge_saves')
                 .update({ 
                     data: gameData,
-                    'created_at': currentTimestamp
                 })
                 .eq('pioneer_name', userId);
 
@@ -88,7 +85,7 @@ export async function saveGameToCloud(gameData, type) {
         } else {
             const { error: insertError } = await supabase
                 .from('CosmicForge_saves')
-                .insert([{ pioneer_name: userId, data: gameData, 'created_at': currentTimestamp }]);
+                .insert([{ pioneer_name: userId, data: gameData }]);
 
             if (insertError) {
                 throw insertError;
